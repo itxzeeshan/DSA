7ddd7296a078d00b07ceb3a4321243ebf703e2aa
Zeeshan Razzaq <zeeshan66780@gmail.com>
2026-01-13 21:21:10 +0500

    comment

---
+++ Pointers/code5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+
+    int* ptr1;
+    int* ptr2 = ptr1;
+
+    cout << ptr1 <<endl;
+    cout << ptr2 <<endl;
+
+    cout << (ptr1 != ptr2) <<endl;
+    return 0;
+}
